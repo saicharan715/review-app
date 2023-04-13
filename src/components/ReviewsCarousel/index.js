41aa79b749cafcf5b96f51c2db79bd32178d0851
saicharan <charanbhupala11@gmail.com>
2023-04-13 10:55:52 +0000

    failed 4 test cases

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,116 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+const reviewsList = [
+  {
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/wade-warren-img.png',
+    username: 'Wade Warren',
+    companyName: 'Rang',
+    description:
+      'The most important thing I learnt is that nothing is a failure, but what we learn from that is a rich and rewarding experience.',
+  },
+  {
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/adrian-williams-img.png',
+    username: 'Adrian Williams',
+    companyName: 'WheelO',
+    description:
+      'Coming to Startup School is the best thing that has happened to me. I wish every startup in the country should get this opportunity.',
+  },
+  {
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/sherry-jhonson-img.png',
+    username: 'Sherry Johnson',
+    companyName: 'MedX',
+    description:
+      'I am glad to have such experienced mentors guiding us in every step through out the 4 weeks. I have improved personally and developed many interpersonal skills.',
+  },
+  {
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/ronald-jones-img.png',
+    username: 'Ronald Jones',
+    companyName: 'Infinos Tech',
+    description:
+      'I am really loving the way how mentors are taking care of us, the way they are explaining big theories with lots of case studies and innovative methods.',
+  },
+]
+class ReviewsCarousal extends Component {
+  state = {
+    activeReviewIndex: 0,
+  }
+
+  getReview = reviewsList => {
+    const {activeReviewIndex} = this.state
+    const {imgUrl, username, companyName, description} = reviewsList[
+      activeReviewIndex
+    ]
+
+    return (
+      <div className="review-container">
+        <h1 className="heading">Reviews</h1>
+        <img src={imgUrl} alt={username} />
+        <p className="username">{username}</p>
+        <p>{companyName}</p>
+        <p>{description}</p>
+      </div>
+    )
+  }
+
+  onLeft = () => {
+    const {activeReviewIndex} = this.state
+
+    if (activeReviewIndex > 0) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex - 1,
+      }))
+    }
+  }
+
+  onRight = () => {
+    const {activeReviewIndex} = this.state
+
+    if (activeReviewIndex < reviewsList.length - 1) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex + 1,
+      }))
+    }
+  }
+
+  render() {
+    const {activeReviewIndex} = this.state
+    const {reviewsList} = this.props
+
+    console.log(activeReviewIndex)
+
+    return (
+      <div className="app-container">
+        <button
+          className="btn"
+          type="button"
+          date-testid="leftArrow"
+          onClick={this.onLeft}
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+            alt="left arrow"
+            className="arrow"
+          />
+        </button>
+        {this.getReview(reviewsList)}
+        <button
+          className="btn"
+          type="button"
+          data-testid="rightArrow"
+          onClick={this.onRight}
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+            alt="right arrow"
+            className="arrow"
+          />
+        </button>
+      </div>
+    )
+  }
+}
+
+export default ReviewsCarousal
